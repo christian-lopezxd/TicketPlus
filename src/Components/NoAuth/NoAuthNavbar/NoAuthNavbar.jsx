9fffeab97ae2b625d634790df024a6444dbee68a
christian-lopezxd <00179320@uca.edu.sv>
2023-07-05 00:58:15 -0600

    fixed navbar

--- src/Components/NoAuth/NoAuthNavbar/NoAuthNavbar.jsx
+++ src/Components/NoAuth/NoAuthNavbar/NoAuthNavbar.jsx
@@ -5,7 +5,7 @@ import LoginButton from "./LoginButton/LoginButton";
 const NoAuthNavbar = () => {
   return (
     <div className="bg-grissoft h-20 flex justify-between p-2 items-center sticky top-0 z-50">
-      <NavLink to="/"><p className="text-white font-lilita text-5xl">TicketPlus</p></NavLink>
+      <NavLink to="/"><p className="text-white font-lilita text-5xl md:text-2xl">TicketPlus</p></NavLink>
       <LoginButton />
       
     </div>
